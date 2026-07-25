7e5ec2574203b44653ceaa92c7c948033050159f
eng-haider <31736386+eng-haider@users.noreply.github.com>
2026-07-25 22:07:35 +0300

    Update TransactionResource.php

--- app/Filament/Resources/Transactions/TransactionResource.php
+++ app/Filament/Resources/Transactions/TransactionResource.php
@@ -6,17 +6,22 @@ use App\Filament\Resources\Transactions\Pages\CreateTransaction;
 use App\Filament\Resources\Transactions\Pages\EditTransaction;
 use App\Filament\Resources\Transactions\Pages\ListTransactions;
 use App\Models\Transaction;
+use BackedEnum;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Support\Icons\Heroicon;
 use Filament\Tables;
 use Filament\Tables\Table;
 
 class TransactionResource extends Resource
 {
     protected static ?string $model = Transaction::class;
-    protected static $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedDocumentText;
+
     protected static ?string $navigationLabel = 'المعاملات';
+
     protected static ?int $navigationSort = 10;
 
     public static function getModelLabel(): string
